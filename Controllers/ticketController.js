e3ec44dba47104bbb0e56f7002261e7bb2615eef
humaisawan <humaisawan0@gmail.com>
2024-09-11 00:50:45 +0500

    changed redirect error and success url

--- Controllers/ticketController.js
+++ Controllers/ticketController.js
@@ -30,9 +30,9 @@ exports.bookTicket = catchAsync(async (req, res, next) => {
       "https://channels-service-alpha.fourvenues.com/tickets/checkout",
       {
         redirect_url:
-          "http://ec2-35-171-3-147.compute-1.amazonaws.com/tickets/success",
+          "https://api.mamvoapp.com/tickets/success",
         error_url:
-          "http://ec2-35-171-3-147.compute-1.amazonaws.com/tickets/fail",
+          "https://api.mamvoapp.com/tickets/fail",
         //send_resourcers: true,
         // metadata:{
         //   userId: req.user._id
